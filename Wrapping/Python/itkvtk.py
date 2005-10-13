24d7fd37ae9615c181fab7620e2c34e3de58ae6d
Gaëtan Lehmann <gaetan.lehmann@jouy.inra.fr>
2005-10-13 11:26:04 +0200

    migration continued
    darcs-hash:20051013092604-2fc9d-3624ae2001327347d80bd608e71dd3c261602e65.gz

--- Wrapping/Python/itkvtk.py
+++ Wrapping/Python/itkvtk.py
@@ -1,6 +1,13 @@
 
-# required to avoid unknown class warnings
-import itk; del itk
+import itk
+import ItkVtkPy
 
+names = [name for name in dir(ItkVtkPy) if not name.startswith("__")]
+for name in names :
+    setattr(itk, name, ItkVtkPy.__dict__[name])
+    
+# some cleanup
+del itk, ItkVtkPy, names, name
+
+# also keep ItkVtkPy members in that namespace
 from ItkVtkPy import *
-
